35fea53022beb7bf7ab93c4a07d60d15f994e8bc
cheile01 <cheile01@dcs.bbk.ac.uk>
2014-12-02 19:06:23 +0000

    initialised Building interface

---
+++ src/Testcases/BuildingTest.java
@@ -0,0 +1,14 @@
+package Testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuildingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
