d9904872b41d9f4d46370525650023560c0f2cde
= <=>
2014-12-18 14:14:05 +0000

    customer and controller

---
+++ src/Coursework/Controller.java
@@ -0,0 +1,17 @@
+package Coursework;
+import java.util.Scanner;
+
+
+public class Controller {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		 
+		
+		System.out.print("Set number of floors: ");
+		int inFloors = scanner.nextInt();// exception handling to be added
+		System.out.print("/nSet number of customers:");
+		int inCostumers = scanner.nextInt();// exception handling to be added
+		
+		
+	}
+}
